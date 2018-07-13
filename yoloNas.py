3ec66e1bafbf684eec5267f85161a558d9e625af
gp <mi99202006@gmail.com>
2018-07-13 09:15:19 +0800

    model update

--- yoloNas.py
+++ yoloNas.py
@@ -1,17 +1,21 @@
 from keras.applications.nasnet import NASNetLarge, NASNetMobile
-from keras.layers import Dense,Conv2D,Input,Flatten,Conv3D
+from keras.layers import Dense,Conv2D,Input,Flatten,MaxPooling2D
 from keras.models import Model
 from keras import losses
 
 import keras.backend as K
 import tensorflow as tf
 import sys
+import cv2
+
+
 
 def loss_fn(y_true, y_pred):
     #y_true = tf.squeeze(y_true)
     #y_true = tf.cast(y_true, dtype=tf.int32)
     #return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
     #                                        labels=y_true, logits=y_pred))
+    
     y_pred = K.reshape(y_pred, (-1, K.int_shape(y_pred)[-1]))
     log_softmax = tf.nn.log_softmax(y_pred)
 
@@ -46,7 +50,7 @@ def build_model_flat(input_image):
     flat=Flatten()(base_model.output)
     fc_1=Dense(4096,activation='relu')(flat)
     output=Dense(10*10,activation='sigmoid')(fc_1)
-    model=Model(input=input_tensor,output=output)
+    model=Model(inputs=input_tensor,outputs=output)
     model.compile(optimizer='adam',loss=losses.binary_crossentropy)
     return model
 
@@ -54,11 +58,38 @@ def build_model_conv(input_image):
 
     input_tensor=Input(shape=(input_image))
     base_model = NASNetLarge(input_shape=input_image,input_tensor=input_tensor,include_top = False,weights=None)
-    conv_1=Conv2D(input_shape=(160,160,409),filters=1,kernel_size=(1,1),activation='sigmoid',padding='valid')(base_model.output)
-    flat=Flatten()(conv_1)
-    model=Model(input=input_tensor,output=conv_1)
+    
+        
+    conv_1=Conv2D(input_shape=(160,160,4033),filters=1,kernel_size=(1,1),activation='sigmoid',padding='valid')(base_model.output)
+    maxpool=MaxPooling2D(pool_size=(4,4))(conv_1)
+    model=Model(inputs=input_tensor,outputs=maxpool)
     model.compile(optimizer='adam',loss=losses.binary_crossentropy)
     return model
 
-model=build_model_conv([5120,5120,3])
-model.summary()
+def easy_model(input_image):
+    input_tensor=Input(shape=(input_image))
+    conv_1=Conv2D(input_shape=input_image,filters=256,kernel_size=(3,3))(input_tensor)
+    conv_2=Conv2D(filters=256,kernel_size=(3,3))(conv_1)
+    conv_3=Conv2D(filters=256,kernel_size=(3,3))(conv_2) 
+    flat=Flatten()(conv_3)
+    output=Dense(1024)(flat)
+    model=Model(inputs=input_tensor,outputs =output)
+    model.compile(optimizer='adam',loss=losses.binary_crossentropy)
+    return model    
+
+#model=build_model_conv([5120,5120,3])
+#model.summary()
+
+label_row=[]
+input_data=cv2.imread('image.jpg')
+input_data/=255
+label_txt=open('label.txt','r')
+for i in range(40):
+    label_row.append(label_txt.read())
+
+
+if __name__ == "__main__":
+    model=build_model_conv([5120,5120,3])
+    model.summary()
+    model.fit(x=input_data,y=y_label,batch_size=1,epochs=10)(
+    model.predict(x,batch_size=1)
